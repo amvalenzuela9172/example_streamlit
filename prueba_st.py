e215bb06e133afbe793c36303cb0d22ee665f411
Ale <alejandro.medinavalenzuela@uts.edu.au>
2026-07-20 22:45:56 +1000

    added new feature

--- prueba_st.py
+++ prueba_st.py
@@ -9,7 +9,7 @@ precio = st.number_input("add your salary ($)", min_value=0.0, value=0.0)
 impuesto = st.slider("Tax (%)", 0, 100, 21)
 
 # Calculation
-precio_final = precio * (1 + impuesto / 100)
+precio_final = precio * (1 - impuesto / 100)
 
 # Output
 if client_name:
