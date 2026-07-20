ee4dd1a0371f831aafbbe41f38863a21886cb037
Ale <alejandro.medinavalenzuela@uts.edu.au>
2026-07-20 22:42:58 +1000

    added new feature

--- prueba_st.py
+++ prueba_st.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 # Page title
-st.title("💰 Tax Calculator Dashboard")
+st.title("💰 Tax Calculator for my baby")
 
 # User inputs
-client_name = st.text_input("Client Name")
-precio = st.number_input("Base Price ($)", min_value=0.0, value=0.0)
+client_name = st.text_input("Add your Name")
+precio = st.number_input("add your salary ($)", min_value=0.0, value=0.0)
 impuesto = st.slider("Tax (%)", 0, 100, 21)
 
 # Calculation
@@ -14,9 +14,9 @@ precio_final = precio * (1 + impuesto / 100)
 # Output
 if client_name:
     st.success(
-        f"Hello **{client_name}**! The final price including tax is **${precio_final:.2f}**."
+        f"Hello **{client_name}**! Your salary after tax is **${precio_final:.2f}**, so your are poor again."
     )
 else:
     st.info(
-        f"The final price including tax is **${precio_final:.2f}**."
+        f"Your salary after tax is **${precio_final:.2f}**, so your are poor again."
     )
